fe1e9d1f7f51887af2e8d4565da101f4a7d3890a
Mavrick Andrianony <mavrickandrianony@MacBook-Air-de-Mavrick.local>
2019-10-14 14:27:11 +0200

    init the project

---
+++ server/src/main/java/com/cashmanager/CashManager/CashManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cashmanager.CashManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashManagerApplication.class, args);
+	}
+
+}
