96b4346275d12cdd9a60df636c9baa33c813af0a
Mavrick Andrianony <mavrickandrianony@MacBook-Air-de-Mavrick.local>
2019-12-02 16:08:19 +0100

    Max attempt, timer 30s

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,6 @@
 INSERT INTO userapp VALUES (5000, 'Test@test.fr', 'Test', 'test');
-INSERT INTO product VALUES (3, 'Coca', 0.90);
-INSERT INTO product VALUES (6, 'Ketchup', 1.50);
+INSERT INTO product VALUES (1, 'Coca', 0.90);
+INSERT INTO product VALUES (2, 'Ketchup', 1.50);
+INSERT INTO product VALUES (3, 'Salad', 2.99);
+INSERT INTO product VALUES (4, 'Sugar', 1.20);
 
